87409ee0f6b79c8b6bd858079c45f11d1443aa3e
fadikadi <elkadifadi@gmail.com>
2025-05-27 07:31:55 -0400

    Update build with correct base URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Makes assets load correctly in production
+  base: '/Dare-to-Win/', // Base URL for GitHub Pages
 })
